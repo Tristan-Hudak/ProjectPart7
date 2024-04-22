79fbbc4719c24adb619da207e69c86bab8ab0180
Tristan-Hudak <tristanj.hudak@gmail.com>
2024-04-22 19:15:41 -0400

    test - create the new modal on character creator 3

--- public/scrpit-character.js
+++ public/scrpit-character.js
@@ -1,12 +1,20 @@
 
-const showModal = (e) => {
-    console.log(e.target.id);
+const showModal = (id) => {
+    if(id == "file"){
+        console.log("in",id)
+        document.getElementById("character-modal").style.display = "block";
+    }
+    if(id == "delete"){
+        console.log("in",id)
+        document.getElementById("delete-modal").style.display = "block";
+    }
+    
 
 };
 
 
 
-document.getElementById("file").onclick = showModal();
-document.getElementById("delete").onclick = showModal();
-document.getElementById("rest").onclick = showModal();
-document.getElementById("level-up").onclick = showModal();
+document.getElementById("file").onclick = showModal("file");
+document.getElementById("delete").onclick = showModal("delete");
+document.getElementById("rest").onclick = showModal("rest");
+document.getElementById("level-up").onclick = showModal("level-up");
