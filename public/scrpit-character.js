cc0aed1d21da232758dead136e24d01783c98129
Tristan-Hudak <tristanj.hudak@gmail.com>
2024-04-22 20:06:40 -0400

    test - create the new modal on character creator 5

--- public/scrpit-character.js
+++ public/scrpit-character.js
@@ -3,12 +3,11 @@ const showModal = (id) => {
     if(id == "file"){
         console.log("in",id)
         document.getElementById("character-modal").style.display = "block";
-        document.getElementById("character-modal").id = "character-modal-click"
+
     }
     if(id == "delete"){
         console.log("in",id)
         document.getElementById("delete-modal").style.display = "block";
-        document.getElementById("delete-modal").id = "delete-modal-click"
     }
     else{
         return;
@@ -17,11 +16,10 @@ const showModal = (id) => {
 };
 
 const hideModal = () => {
+    console.log("say hello")
     document.getElementById("delete-modal").style.display = "none";
     document.getElementById("character-modal").style.display = "none";
 
-    document.getElementById("character-modal-click").id = "character-modal"
-    document.getElementById("delete-modal-click").id = "delete-modal"
 }
 
 
@@ -39,5 +37,5 @@ document.getElementById("level-up").onclick = function(){
     showModal("level-up");
 }
 
-document.getElementById("close_box").onclick = hideModal();
+document.getElementById("create-character").onclick = hideModal();
 
