6808b578b976241dc7ce739ad5270ebf4291d08e
Tristan-Hudak <tristanj.hudak@gmail.com>
2024-04-23 22:32:33 -0400

    test - try lvl up 11

--- public/scrpit-character.js
+++ public/scrpit-character.js
@@ -454,9 +454,9 @@ const lvlCharacter = async(character) =>{
             form.character_cha.value = char.character_cha;
             //console.log(form.character_prof, char.character_prof, char.character_con[0]);
             form.charcater_prof = char.character_prof
-            console.log(form.character_hp, char.character_hp + ( checkClassHp(char.character_class, char.character_con[0]) / 2), char.character_lvl[0] + 1)
+            console.log(form.character_hp, char.character_hp + ( checkClassHp(char.character_class, char.character_con[0].value) / 2), char.character_lvl[0].value + 1)
 
-            form.character_hp.value = char.character_hp + ( checkClassHp(char.character_class, char.character_con[0]) / 2);
+            form.character_hp.value = char.character_hp.value + ( checkClassHp(char.character_class, char.character_con[0]) / 2);
             form.character_lvl.value = char.character_lvl[0] + 1;
 
             makeNewCharacter();
@@ -640,7 +640,7 @@ const modDetect = (thing) =>{
 
 
 const checkClassHp = async(cls, con) => {
-    //console.log(cls, con)
+    console.log(cls, con)
 
     const value = modDetect(con)
 
