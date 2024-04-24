38808cc1d55652096a8f740afd4945104d07df90
Tristan-Hudak <tristanj.hudak@gmail.com>
2024-04-23 23:41:23 -0400

    test - why is profs not working 2

--- public/scrpit-character.js
+++ public/scrpit-character.js
@@ -454,8 +454,12 @@ const lvlCharacter = async(character) =>{
             form.character_cha.value = char.character_cha;
             console.log(char.character_class, char.character_con);
             value = await checkClassHp(char.character_class, char.character_con);
-            console.log(popProf(char,character_prof))
+
+
+            console.log(popProf(char.character_prof), char.character_prof)
             form.charcater_prof = popProf(char.character_prof);
+
+
             console.log(value, +char.character_lvl + 1, +char.character_hp + ( value / 2))
 
             form.character_hp.value = +char.character_hp + ( value / 2);
